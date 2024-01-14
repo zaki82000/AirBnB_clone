5c2790ef8f0857206e1b2ef452429e3e0a8ce40c
zaki82000 <yabdelshafy0@gmail.com>
2024-01-14 16:35:09 +0200

    mend
    add airbnb file

--- models/base_model.py
+++ models/base_model.py
@@ -10,28 +10,26 @@ class BaseModel:
     """
     class BaseModel that defines all common attributes/methods for other clases
     """
-    def __init__(self, *args, **kwargs):
-        if kwargs:  # Check if kwargs is not empty
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value) 
-        else:
+    def __init__(self):
+            self.created_at = datetime.now()
             self.id = str(uuid.uuid4())
-            self.updated_at = self.created_at = datetime.now()
+            self.updated_at = self.created_at
             """
             Private instance attribute: height and width
             """
 
     def __str__(self):
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        obj = self.__dict__.copy()
-        obj['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        obj['id'] = self.id
-        obj['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        obj['__class__'] = self.__class__.__name__
-        return obj
+        return  {
+        "my_number": self.my_number,
+        "name": self.name,
+        "__class__": self.__class__.__name__,
+        "created_at": self.created_at.isoformat(),
+        "id": self.id,
+        "updated_at": self.updated_at.isoformat(),
+    }
