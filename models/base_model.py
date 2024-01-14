925153c0c53cc7c76714f706b4766f363b426d7d
zaki82000 <yabdelshafy0@gmail.com>
2024-01-14 17:07:49 +0200

    mend
    add airbnb file

--- models/base_model.py
+++ models/base_model.py
@@ -11,25 +11,24 @@ class BaseModel:
     class BaseModel that defines all common attributes/methods for other clases
     """
     def __init__(self):
-            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
-            self.updated_at = self.created_at
+            self.created_at = datetime.now()
+            
             """
             Private instance attribute: height and width
             """
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        return  {
-        "my_number": self.my_number,
-        "name": self.name,
-        "__class__": self.__class__.__name__,
-        "created_at": self.created_at.isoformat(),
-        "id": self.id,
-        "updated_at": self.updated_at.isoformat(),
-    }
+        obj = self.__dict__.copy()
+        obj['__class__'] = self.__class__.__name__
+        obj['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        obj['id'] = self.id
+        obj['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return obj
