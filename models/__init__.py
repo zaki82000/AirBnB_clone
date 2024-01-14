a56fe4b795455a542687b4751873f9774c8689aa
zaki82000 <yabdelshafy0@gmail.com>
2024-01-15 00:13:09 +0200

    mend
    add airbnb file

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,23 @@
 """
+[BaseModel] (adca1aea-b8e6-4de0-b0d5-1f9a5750f633) {'id': 'adca1aea-b8e6-4de0-b0d5-1f9a5750f633', 'created_at': datetime.datetime(2024, 1, 14, 23, 0, 36, 159965), 
+'updated_at': datetime.datetime(2024, 1, 14, 23, 0, 36, 159980), 'name': 'My First Model', 'my_number': 89}
+[BaseModel] (adca1aea-b8e6-4de0-b0d5-1f9a5750f633) {'id': 'adca1aea-b8e6-4de0-b0d5-1f9a5750f633', 'created_at': datetime.datetime(2024, 1, 14, 23, 0, 36, 159965), 
+'updated_at': datetime.datetime(2024, 1, 14, 23, 0, 36, 160910), 'name': 'My First Model', 'my_number': 89}
+{'id': 'adca1aea-b8e6-4de0-b0d5-1f9a5750f633', 'created_at': '2024-01-14T23:00:36.159965', 'updated_at': '2024-01-14T23:00:36.160910', 'name': 'My First Model', 'my_number': 89, '__class__': 'BaseModel'}
+JSON of my_model:
+        id: (<class 'str'>) - adca1aea-b8e6-4de0-b0d5-1f9a5750f633
+        created_at: (<class 'str'>) - 2024-01-14T23:00:36.159965
+        updated_at: (<class 'str'>) - 2024-01-14T23:00:36.160910
+        name: (<class 'str'>) - My First Model
+        my_number: (<class 'int'>) - 89
+        __class__: (<class 'str'>) - BaseModel
 
----------------------------------------------------------------
-[BaseModel] (b6a6e15c-c67d-4312-9a75-9d084935e579) {'my_number': 89, 'name': 'My First Model', 'updated_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119434), 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119427)}
-[BaseModel] (b6a6e15c-c67d-4312-9a75-9d084935e579) {'my_number': 89, 'name': 'My First Model', 'updated_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119572), 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119427)}
+###########################################################
+
+[BaseModel] (b6a6e15c-c67d-4312-9a75-9d084935e579) {'my_number': 89, 'name': 'My First Model', 
+'updated_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119434), 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119427)}
+[BaseModel] (b6a6e15c-c67d-4312-9a75-9d084935e579) {'my_number': 89, 'name': 'My First Model', 
+'updated_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119572), 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119427)}
 {'my_number': 89, 'name': 'My First Model', '__class__': 'BaseModel', 'updated_at': '2017-09-28T21:05:54.119572', 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': '2017-09-28T21:05:54.119427'}
 JSON of my_model:
     my_number: (<class 'int'>) - 89
--- models/base_model.py
+++ models/base_model.py
@@ -30,8 +30,8 @@ class BaseModel:
             """
 
     def __str__(self):
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+    
     def save(self):
         self.updated_at = datetime.now()
 
