4f15697fb4543482f297465dd83df27d445a7ab1
zaki82000 <yabdelshafy0@gmail.com>
2024-01-16 22:17:10 +0200

    mend
    update

--- console.py
+++ console.py
@@ -5,13 +5,23 @@ Contains the definition of 'HBNBCommand' class.
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.amenity import Amenity
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
     classes = (
         'BaseModel',
-        'User'
+        'User',
+        'City',
+        'Place',
+        'Review',
+        'State',
+        'Amenity'
         )
 
     def do_create(self, line):
