6c8513291764aedb4d5bd2824f65fddad97ca388
zaki82000 <yabdelshafy0@gmail.com>
2024-01-16 11:12:43 +0200

    add console.py

---
+++ console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)'
+
+    def do_quit(self, arg):
+        return True
+
+    def do_EOF(self, line):
+        return True
+
+    def emptyline(self):
+        return cmd.Cmd.emptyline(self)
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
