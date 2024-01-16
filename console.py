e28701f64ed36343fdf0900cebffe12f23aa90e2
zaki82000 <yabdelshafy0@gmail.com>
2024-01-16 21:09:17 +0200

    update file

--- console.py
+++ console.py
@@ -3,10 +3,34 @@
 Contains the definition of 'HBNBCommand' class.
 """
 import cmd
+from models.base_model import BaseModel
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
+    classes = (
+        'BaseModel',
+        'User'
+        )
+
+    def do_create(self, line):
+        if line:
+            if line not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+            else:
+                instance = eval(f"{line}()")
+                instance.save()
+                print(instance.id)
+        else:
+            print("** class name missing **")
+
+    def to_show(self, line):
+        if line:
+            if line not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+            else:
+                instance = globals()[__class__.__name__].id
 
     def emptyline(self):
         pass
